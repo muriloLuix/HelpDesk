b89a66e09643bc6c995b679489e75f5853f2ab05
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-02-27 17:06:15 -0300

    Adding DarkMode (Not finalized)

---
+++ assets/js/theme.js
@@ -0,0 +1,7 @@
+// function switchTheme (){
+//     document.body.classList.toggle('is-orange')
+//     document.body.classList.toggle('is-dark')
+// }
+
+
+// document.getElementById('switchBtn').addEventListener('click', switchTheme)
